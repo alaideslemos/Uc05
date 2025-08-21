f366ffa4639addad2e9c495f89c8f675c2eb60a8
alaideslemos <alaideslemos40@gmail.com>
2025-08-21 20:53:48 -0300

    ExemploWhile
    A classe ExemploWhile foi adicionada

---
+++ MiniMarket/src/app/ExemploWhile.java
@@ -0,0 +1,18 @@
+package app;
+
+public class ExemploWhile {
+
+	public static void main(String[]args) {
+		
+		int contador = 1;
+		
+		while (contador <= 5) {
+			
+			System.out.println("O contador agora é:" + contador);
+			
+			contador = contador + 1;
+			
+		}
+		System.out.println("Fila de contagem!");
+	}
+}
